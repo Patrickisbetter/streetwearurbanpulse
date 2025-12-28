ad5605e439550616ab642aa01b421fc6ab38775a
Patrickisbetter <108005794+Patrickisbetter@users.noreply.github.com>
2025-12-28 06:00:12 -0500

    Add files via upload

--- anotherscript.js
+++ anotherscript.js
@@ -12,149 +12,126 @@ newsline.ontransitionend = (event) => {
 }
 
 
-function moving_elements(parent, spacing, direction = "left") {
+function moving_elements(parent, spacing, ...args) {
+    spacing = Math.abs(spacing);
+
+    let direction = "left";
+    let target = "";
+    
+    args.forEach((item) => {
+        if (item === null) {return;}
+        if (typeof item === "string") {
+            if (item === "left" || item === "right") {direction = item;}            
+            return;
+        }
+        if (typeof item === "object") {
+            if (!target && item.nodeType === Node.ELEMENT_NODE) {target = item;}
+            return;
+        }
+
+    })
+    
     let left_end = window.getComputedStyle(parent.children[parent.childElementCount-1]).translate;
     left_end = Number(left_end.slice(0, left_end.length-1));
     let right_end = window.getComputedStyle(parent.children[parent.childElementCount-2]).translate;
     right_end = Number(right_end.slice(0, right_end.length-1));
     let unit = window.getComputedStyle(parent.children[0]).translate.slice(-1);
-    console.log(parent, left_end, right_end)
+    let translate_holder = [["from",[]],["to",[]]];
     Array.from(parent.children).forEach((item) => {
-        item.style.translate = window.getComputedStyle(item).translate
-        if (left_end > Number(item.style.translate.slice(0, item.style.translate.length-1))) {
+        item.style.translate = window.getComputedStyle(item).translate;
+        translate_holder[0][1].push(item.style.translate)
+        if (left_end >= Number(item.style.translate.slice(0, item.style.translate.length-1))) {
             left_end = Number(item.style.translate.slice(0, item.style.translate.length-1));
         }
-        if (right_end < Number(item.style.translate.slice(0, item.style.translate.length-1))) {
+        if (right_end <= Number(item.style.translate.slice(0, item.style.translate.length-1))) {
             right_end = Number(item.style.translate.slice(0, item.style.translate.length-1));
         }
-        
-        
-        
-        
     });
-    
-    
-
+    if (target) {
+        let displacement;
+        let tempcount = 0;
+        let tempitem;
+        for(let index=0; index < parent.childElementCount; index++) {
+            tempitem = parent.children[index]
+            if(tempitem === target) {displacement = index; tempcount++;}
+            if(tempitem.classList.contains("active")) {displacement -= index; tempcount++;}
+            if(tempcount==2) {break;} 
+        }
+        tempcount = null;
+        tempitem = null;
+        displacement/=Math.abs(displacement);
+        target.style.transition = "none"
+        target.style.contentVisibility = "hidden"
+        target.style.translate = spacing * displacement + "%"
+        target.style.transition = ""
+        target.style.contentVisibility = ""        
+        target.style.translate = "0%"
+        parent.querySelector(".active").style.translate = spacing * -displacement + "%"
+        
+        return translate_holder;
+    }
     if (direction == "left") {
+        let translate_number;
         Array.from(parent.children).forEach((item) => {
             translate_number = Number(item.style.translate.slice(0,item.style.translate.length-1))
-            console.log(translate_number)
-            if (translate_number == left_end) {item.style.contentVisibility = "hidden"; item.style.translate = right_end+"%"; return;}
+            if (translate_number == left_end) {
+                item.style.contentVisibility = "hidden"; item.style.translate = right_end+"%"; 
+                return;
+            }
             item.style.translate = (translate_number-spacing) + "%";
             if (translate_number == 0 || translate_number == spacing) {
                 item.style.contentVisibility = "";
             }
-            
-            
-       })
-        
-        
-        
-        
-        
+       }) 
     } else if (direction == "right") {
 
     }
-    
+    return translate_holder;
 }
 
 
 let carousel_counter = 0;
 carousel.children[carousel_counter].classList.add("active");
-moving_elements(carousel, 100);
+carousel_overlay.querySelectorAll("button")[carousel_counter].classList.add("active");
+console.log(moving_elements(carousel, -100)[0][1]);
 carousel.querySelector(".active").ontransitionend = (event) => {
-    console.log(event)
     moving_elements(carousel, 100);
+    carousel_overlay.querySelectorAll("button").forEach((item) => {item.style.pointerEvents = ""});
+    Array.from(carousel.children).forEach((item) => {item.style.transition = ""});
 
 
 }
 
 carousel.querySelector(".active").ontransitionstart = (event) => {
     carousel.children[carousel_counter].classList.remove("active");
+    carousel_overlay.querySelectorAll("button")[carousel_counter].classList.remove("active");
+    carousel_overlay.querySelectorAll("button").forEach((item) => {item.style.pointerEvents = "none"});
 
     carousel_counter++;
     carousel_counter%=3;
     carousel.children[carousel_counter].classList.add("active");
-    
+    carousel_overlay.querySelectorAll("button")[carousel_counter].classList.add("active")
+
     
 }
-
-
-/*let carousel_counter = 0;
-
-carousel.children[carousel_counter].style.transform = "translate(-100%)";
-carousel.children[carousel_counter+1].style.transform = "translate(0%)";
-carousel.children[carousel_counter].classList.add("active");
-carousel_overlay.querySelector('div').children[carousel_counter].classList.add("active");
-
-console.log("yay1");
-
-carousel.querySelector(".active").ontransitionend = (event) => {
-    carousel.children[carousel_counter].classList.remove("active");
-    carousel_overlay.querySelector('div').children[carousel_counter].classList.remove("active");
-
-    Array.from(carousel.children).forEach((item, index) => {
-        index = carousel.childElementCount - 1 - index;
-        item = carousel.children[index];
-        carousel_counter++;
-        carousel_counter%=carousel.childElementCount;
-        item.style.transform = `translate(${100-carousel_counter*100}%)`;
-        if (carousel_counter == 0) {
-            item.style.opacity = 0;
-            item.style.contentVisibility= "hidden"
-        } else {
-            item.style.opacity = 1;
-            item.style.contentVisibility = ""
-        }
-    });
-
-    carousel_counter++;
-    carousel_counter%=carousel.childElementCount;
-    carousel.children[carousel_counter].classList.add("active");
-    carousel_overlay.querySelector('div').children[carousel_counter].classList.add("active");
-    carousel_overlay.querySelectorAll("button").forEach((item) => {item.style.pointerEvents = ""});
-    return;
-}
-carousel.querySelector(".active").ontransitionstart = (event) => {
-    carousel_overlay.querySelectorAll("button").forEach((item) => {item.style.pointerEvents = "none"}
-);
-}*/
-
-/*
-let shift;
-
+let translate_holder;
 carousel_overlay.querySelectorAll("button").forEach((item, index) => {
     item.onclick = (event) => {
-        console.log("clicked!!")
-        shift = index - Array.from(carousel_overlay.querySelectorAll("button")).indexOf(carousel_overlay.querySelector(".active"))
-        let overlay_counter = (carousel_counter + 2)%3
-        if (shift == Math.abs(shift)) {
-            
-            Array.from(carousel.children).forEach((picture, index) => {
-                    index = carousel.childElementCount - 1 - index;
-                    picture = carousel.children[index];
-                    if(picture.className.includes("active")) {
-                        console.log(picture)
-                        picture.style.transition = "none";
-                        picture.style.transform = `translate(${-100+index*100}%)`;
-                        picture.style.transition = "";
-                        overlay_counter++;
-                        overlay_counter%=3;
-                    } else {
-                        //window.getComputedStyle(picture).transform;
-                        picture.style.transition = "none";
-                        picture.style.transform = `translate(${100-overlay_counter*100}%)`;
-                        overlay_counter++;
-                        overlay_counter%=3;
-                    }
-            });
-            
-            
-        }
-    }
-})
-*/
+        if (event.target.classList.contains("active")) {return;}
+        carousel_overlay.querySelectorAll("button").forEach((item) => {item.style.pointerEvents = ""});
+        translate_holder = moving_elements(carousel, 100, carousel.children[index])[0][1].forEach((item) => {return 0;})
+        carousel_counter = (index+1)%3;
+        Array.from(carousel.children).forEach((item) => {item.style.transition = "1s ease-in-out translate"})
 
+        
+        
+
+    }
+    
+
+})
+
+/*
 $("#inspo > h1").css("background-position-y", "100%")
 $("#inspo > h1").on("transitionend", (event) => {
     if ($("#inspo > h1").css("background-position-y") == "0%") {
@@ -163,3 +140,4 @@ $("#inspo > h1").on("transitionend", (event) => {
         $("#inspo > h1").css("background-position-y", "0%")
     }
 })
+*/
